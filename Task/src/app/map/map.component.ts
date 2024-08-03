ed080cdc1b36bb8f360c089093613c3e4153dbfc
kayuh7 <kayquach7@gmail.com>
2024-08-02 21:15:55 -0700

    Part F

--- Task/src/app/map/map.component.ts
+++ Task/src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngOnInit() {
+    const paths = this.el.nativeElement.querySelectorAll('path');
+    paths.forEach((path: SVGPathElement) => {
+      this.renderer.listen(path, 'click', (event) => this.onCountryClick(event, path.getAttribute('name')));
+    });
+  }
+
+  onCountryClick(event: MouseEvent, countryName: string | null) {
+    if (countryName) {
+    console.log('Country clicked:', countryName);
+    }
+  }
 }
